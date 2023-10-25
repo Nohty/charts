eb85a48dc0e6385772d519b19c560d65dac69c05
Daan Hautekiet <daanhautekiet@gmail.com>
2023-10-25 16:23:58 +0200

    feat: create CandleStick class

--- src/lib/chart.ts
+++ src/lib/chart.ts
@@ -1,13 +1,11 @@
-import { drawText } from "./graphics";
-import { remap } from "./math";
-import { ChartOptions, Data, DeepPartial, InputData } from "./types";
+import { Bounds, ChartOptions, DataPoint, DeepPartial } from "./types";
 
-export class ChartCandleStick {
+export class Chart {
   private options: ChartOptions;
   private canvas: HTMLCanvasElement;
   private ctx: CanvasRenderingContext2D;
 
-  private data: Data[] = [];
+  private data: CandleStick[] = [];
 
   private margin = 20;
 
@@ -35,85 +33,56 @@ export class ChartCandleStick {
     this.ctx = this.canvas.getContext("2d")!;
   }
 
-  public setData(data: InputData[]): void {
-    this.data = data.map((d) => [new Date(d[0]), d[1], d[2], d[3], d[4]]);
+  private getDataBounds(): Bounds {
+    return {
+      left: Math.min(...this.data.map((d) => d.getDataPoint().time.getTime())),
+      right: Math.max(...this.data.map((d) => d.getDataPoint().time.getTime())),
+      top: Math.max(...this.data.map((d) => d.getDataPoint().high)),
+      bottom: Math.min(...this.data.map((d) => d.getDataPoint().low)),
+    };
+  }
+
+  private getPixelBounds(): Bounds {
+    return {
+      left: this.margin,
+      right: this.canvas.width - this.margin,
+      top: this.margin,
+      bottom: this.canvas.height - this.margin,
+    };
+  }
+
+  private cleansingData(data: DataPoint[]): DataPoint[] {
+    return data.map((d) => {
+      if (d.time instanceof Date) return d;
+      else return { ...d, time: new Date(d.time) };
+    });
+  }
+
+  public setData(data: DataPoint[]): void {
+    console.log(data);
+    this.data = this.cleansingData(data).map((d) => new CandleStick(this.ctx, d));
     this.draw();
   }
 
-  public addData(data: InputData): void {
-    this.data.push([new Date(data[0]), data[1], data[2], data[3], data[4]]);
+  public addData(data: DataPoint[]): void {
+    this.data.push(...this.cleansingData(data).map((d) => new CandleStick(this.ctx, d)));
+    this.draw();
   }
 
   public draw(): void {
-    this.ctx.clearRect(0, 0, this.options.width, this.options.height);
-
-    // Drawing the axes
-    this.drawAxes();
-
-    const yValues = this.data.flatMap((d) => [d[1], d[2], d[3], d[4]]);
-    const yMax = Math.max(...yValues);
-    const yMin = Math.min(...yValues);
-
-    const candleWidth = (this.options.width - 2 * this.margin) / this.data.length;
-
-    for (let i = 0; i < this.data.length; i++) {
-      const [date, open, close, high, low] = this.data[i];
-      const x = this.margin + i * candleWidth;
-
-      const yHigh = remap(yMax, yMin, this.margin, this.options.height - this.margin, high);
-      const yLow = remap(yMax, yMin, this.margin, this.options.height - this.margin, low);
-      const yOpen = remap(yMax, yMin, this.margin, this.options.height - this.margin, open);
-      const yClose = remap(yMax, yMin, this.margin, this.options.height - this.margin, close);
-
-      // Draw the candle body
-      this.ctx.fillStyle = open < close ? "#00ff00" : "#ff0000";
-      this.ctx.fillRect(x, yClose, candleWidth, yOpen - yClose);
-
-      // Draw the wicks
-      this.ctx.strokeStyle = "#000000"; // wicks are usually black
-      this.ctx.beginPath();
-      this.ctx.moveTo(x + candleWidth / 2, yLow);
-      this.ctx.lineTo(x + candleWidth / 2, yHigh);
-      this.ctx.stroke();
-    }
+    const candleStick = this.data[0];
+    candleStick.draw(this.getDataBounds(), this.margin);
   }
 
-  public drawAxes(): void {
-    // Drawing the x-axis (time)
-    drawText(
-      this.ctx,
-      this.options.labels[0],
-      [this.options.width / 2, this.options.height - this.margin / 2],
-      {
-        color: this.options.layout.textColor,
-        size: 12,
-        align: "center",
-        baseline: "middle",
-        fontFamily: "Arial",
-        fontWeight: "normal",
-      }
-    );
+  public drawAxes(): void {}
+}
 
-    this.ctx.strokeStyle = this.options.layout.lineColor;
-    this.ctx.beginPath();
-    this.ctx.moveTo(this.margin, this.options.height - this.margin);
-    this.ctx.lineTo(this.options.width - this.margin, this.options.height - this.margin);
-    this.ctx.stroke();
+class CandleStick {
+  constructor(private ctx: CanvasRenderingContext2D, private data: DataPoint) {}
 
-    // Drawing the y-axis (price)
-    drawText(this.ctx, this.options.labels[1], [this.margin / 2, this.options.height / 2], {
-      color: this.options.layout.textColor,
-      size: 12,
-      align: "center",
-      baseline: "middle",
-      fontFamily: "Arial",
-      fontWeight: "normal",
-    });
-
-    this.ctx.strokeStyle = this.options.layout.lineColor;
-    this.ctx.beginPath();
-    this.ctx.moveTo(this.margin, this.margin);
-    this.ctx.lineTo(this.margin, this.options.height - this.margin);
-    this.ctx.stroke();
+  public getDataPoint(): DataPoint {
+    return this.data;
   }
+
+  public draw(bounds: Bounds, margin: number, width = 10): void {}
 }
