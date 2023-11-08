8b59ca1416f4cc6484e84bc5057e9b7b3ec7a23d
Flor Stellamans <flor.stellamans@student.howest.be>
2023-11-08 15:34:50 +0100

    remove drawing removes everything in the canvas

--- src/lib/paint.ts
+++ src/lib/paint.ts
@@ -4,12 +4,18 @@ export class Paint {
 
     constructor(private ctx: CanvasRenderingContext2D) {}
 
-    public drawLine(startPos: Point, endPoint: Point): void {
+    public draw(startPos: Point, endPoint: Point): void {
         this.ctx.beginPath();
         this.ctx.moveTo(startPos[0], startPos[1]);
         this.ctx.lineWidth = 1;
         this.ctx.lineTo(endPoint[0], endPoint[1]);
         this.ctx.strokeStyle = 'black';
         this.ctx.stroke();
-    }   
+    }
+    
+    public removeDrawings(): void {
+        // removes everything in the canvas
+        this.ctx.clearRect(0, 0, 1200, 900);
+        
+    }
 }
